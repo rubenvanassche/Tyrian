8068b8d343812159a2437cc25893c7c091f86edf
Ruben <rubenvanassche@gmail.com>
2014-12-08 15:55:24 +0100

    SFML Library integration

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
-#include <iostream>
-
-using namespace std;
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    cout << "Hello, World!" << endl;
-    cout << "test git" << endl;
-    return 0;
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+
+    sf::CircleShape shape(50);
+    shape.setFillColor(sf::Color(100, 250, 50));
+
+    while (window.isOpen())
+    {
+
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window : exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        // Clear screen
+        window.clear();
+
+        window.draw(shape);
+
+        // Update the window
+        window.display();
+    }
+    return EXIT_SUCCESS;
 }
